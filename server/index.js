0a9cf0ec5e7264e7f060f038dff10068af3b485e
Siddharth Tiwari <tiwarisiddharth27@gmail.com>
2023-07-24 12:56:18 +0530

    Performance Page Implemented

--- server/index.js
+++ server/index.js
@@ -17,7 +17,9 @@ import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
 import Transaction from "./models/Transaction.js";
 import OverallStat from "./models/OverallStat.js";
-import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat } from "./data/index.js"
+import AffiliateStat from "./models/AffiliateStat.js"
+import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat, dataAffiliateStat } from "./data/index.js"
+
 
 
 // CONFIGURATION
@@ -53,6 +55,7 @@ mongoose.connect(process.env.MONGO_URL, {
     app.listen(PORT, ()=>{
         console.log(`Server port:${PORT}`);
         // only add data once
+        // AffiliateStat.insertMany(dataAffiliateStat)
         // OverallStat.insertMany(dataOverallStat);
         //Product.insertMany(dataProduct);
         // ProductStat.insertMany(dataProductStat);
